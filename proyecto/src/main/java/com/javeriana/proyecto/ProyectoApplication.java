c9014a7ea4a4a34c6a9fff0f497fb5519618de47
Vagui998 <jm.aguiar998@gmail.com>
2023-03-11 19:40:03 -0500

    first commit

---
+++ proyecto/src/main/java/com/javeriana/proyecto/ProyectoApplication.java
@@ -0,0 +1,17 @@
+package com.javeriana.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Proyecto Desarrollo Web: Andres Felipe Duarte, Juan Manuel Aguiar
+
+@SpringBootApplication
+public class ProyectoApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
